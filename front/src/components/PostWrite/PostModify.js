4977d9a1e64078b681e5e02ada19432a6fdb08b0
JeongbinPark <qkrwjdqls99@gmail.com>
2022-03-29 04:18:08 +0900

    feat(post modify): 글쓰기에서 수정 가능한 div 추가하는 기능 추가

--- front/src/components/PostWrite/PostModify.js
+++ front/src/components/PostWrite/PostModify.js
@@ -2,11 +2,11 @@ import React, { useState, useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { CHANGE_VIEWMODE } from '../../_actions/types';
 import { savePostContentsAction } from '../../_actions';
-import PostForm from './PostForm';
+import shortid from 'shortid';
 
 const PostModify = () => {
   const dispatch = useDispatch();
-  const [postContents, ] = useState('');  //object 형태
+  const [postContents, setPostContents] = useState([]);  //object 형태
 
   const changeViewMode = useCallback(()=>{
     //await
@@ -17,10 +17,39 @@ const PostModify = () => {
     })
   }, [])
 
+  // const onChangeBlockObject = () => {
+  //   console.log(postContents);
+  // }
+
+  const makeNewBlockObject = () => {
+    // const target = document.getElementsByClassName('postContentsBody')[0];
+    // let div = document.createElement('div');
+    // div.setAttribute('className', 'blockObject');
+    // div.setAttribute('contentEditable', 'true');
+    // div.addEventListener('ValueChange', onChangeBlockObject);
+    // target.append(div);
+    const newBlockObject = {
+      id: shortid.generate(),
+      type: "paragraph",
+      text: "",
+      children: []
+    }
+    setPostContents([...postContents, newBlockObject]);
+    console.log(postContents);
+  }
+
   return (
   <div className='PostModify'>
     <button onClick={changeViewMode}>미리보기</button>
-    <PostForm />
+    <div className='postContentsBody' >
+      {postContents && postContents?.map( el => (
+        <div key={el.id} className="blockObject" type={el.type} contentEditable="true" suppressContentEditableWarning="true">
+          {el.text}
+        </div>
+        )
+      )}
+    </div>
+   <button onClick={makeNewBlockObject}>+</button>
   </div>
   )
 }
