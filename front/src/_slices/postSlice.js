e44e49c866c35751852ebd893f72b029b5224402
JeongbinPark <qkrwjdqls99@gmail.com>
2022-04-05 03:50:20 +0900

    feat(post read): url 파라미터에 따라 비동기적으로 post 데이터 얻기

--- front/src/_slices/postSlice.js
+++ front/src/_slices/postSlice.js
@@ -1,12 +1,29 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 import { 
-  dummyDataForContents,
   dummyDataForTOC 
 } from './dummydata';
 import shortid from 'shortid';
 
+export const loadPost = createAsyncThunk(
+  "post/loadPost",
+  async (loadPostData) =>{
+    // const response = await axios.post(`http://localhost:8000/api/${loadPostData.userNickname}/posts/${loadPostData.postId}`, loadPostData);
+    const response = await axios.post(`http://localhost:8000/api/getPostData`, loadPostData);
+    return response.data.dummyDataForContents;
+  }
+)
+export const loadPostTOC = createAsyncThunk(
+  "post/loadPostTOC",
+  async (loadPostTOCData) =>{
+    const response = await axios.post(`http://localhost:8000/api/getPostTOCData`, loadPostTOCData);
+    return response.data.dummyDataForTOC;
+  }
+)
+
 const initialState = {
-  value: 0,
+  loadPostStatus: 'idle',
+  loadPostTOC: 'idle',
   activeId : '',
   showPreview: false,
   modifyingPostContents: {
@@ -17,18 +34,20 @@ const initialState = {
   PostData: {
     postId: shortid.generate(),
     title: '',
+    author: {
+      id: shortid.generate(),
+      nickname: ''
+    },
     contents: []
   },
   TOCData : [],
+  error: null,
 }
 
 export const postSlice = createSlice({
   name: 'post',
   initialState,
   reducers: {
-    addValue:(state) => {
-      state.value += 1;
-    },
     changeViewModeToPreview:(state, action) => {
       state.showPreview = action.payload;
     },
@@ -38,9 +57,6 @@ export const postSlice = createSlice({
     setActiveId: (state, action) => {
       state.activeId = action.payload;
     },
-    loadPostContents: (state) => {
-      state.PostData = dummyDataForContents; 
-    },
     loadTempPostContents: (state) => {
       const tempPostContents = state.modifyingPostContents;
       state.PostData = tempPostContents;
@@ -49,6 +65,30 @@ export const postSlice = createSlice({
       state.TOCData = dummyDataForTOC;
     }
   },
+  extraReducers: builder => {
+    builder.addCase(loadPost.pending, (state)=> {
+      state.loadPostStatus = 'loading';
+    })
+    builder.addCase(loadPost.fulfilled, (state, {payload})=> {
+      state.loadPostStatus = 'success';
+      state.PostData = payload;
+    })
+    builder.addCase(loadPost.rejected, (state, action)=> {
+      state.loadPostStatus = 'failed';
+      state.error = action.payload;
+    })
+    builder.addCase(loadPostTOC.pending, (state)=> {
+      state.loadPostTOCStatus = 'loading';
+    })
+    builder.addCase(loadPostTOC.fulfilled, (state, {payload})=> {
+      state.loadPostTOCStatus = 'success';
+      state.TOCData = payload;
+    })
+    builder.addCase(loadPostTOC.rejected, (state, action)=> {
+      state.loadPostTOCStatus = 'failed';
+      state.error = action.payload;
+    })
+  }
 })
 
 export const { 
@@ -56,16 +96,16 @@ export const {
   changeViewModeToPreview,
   savePostContents,
   setActiveId, 
-  loadPostContents, 
   loadTempPostContents, 
-  loadPostTOC 
 } = postSlice.actions;
 
 export default postSlice.reducer;
 
-export const selectValue = (state) => state.post.value;
+export const selectLoadPostStatus = (state) => state.post.loadPostStatus;
 export const selectActiveId = (state) => state.post.activeId;
 export const selectPostData = (state) => state.post.PostData;
 export const selectPostTOC = (state) => state.post.TOCData;
 export const selectShowPreview = (state) => state.post.showPreview;
 export const selectModifyingPostData = (state) => state.post.modifyingPostContents;
+export const selectAuthorNickname = (state) => state.post.PostData.author.nickname;
+export const selectPostId = (state) => state.post.PostData.postId;
--- front/src/components/PostRead/TitleList.js
+++ front/src/components/PostRead/TitleList.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { HashLink } from 'react-router-hash-link';
 import { useSelector } from 'react-redux';
-import { selectActiveId } from '../../_slices/postSlice';
+import { selectActiveId, selectAuthorNickname, selectPostId } from '../../_slices/postSlice';
 
 const TitleList = ({ title }) => {
   const activeId = useSelector(selectActiveId);
+  const authorNickname = useSelector(selectAuthorNickname);
+  const postId = useSelector(selectPostId);
   const active = activeId === title;
 
   return (
-    <HashLink smooth to={`/post#${title}`}>
+    <HashLink smooth to={`/${authorNickname}/posts/${postId}#${title}`}>
       {active && <span className='bookmark'></span>}
       <p className={active ? "active" : ""}>{title}</p>
     </HashLink>
