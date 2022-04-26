965a9d138ea011efb16b2cbe8e8f0c6430132b9d
bjh <bqwert36@hanmail.net>
2022-04-26 14:32:35 +0900

    FIX & ADD : user_auth 테이블 속성 추가로 인한 코드 변경

--- backend/spring/src/main/java/com/example/back/repository/CustomRepository/InsertCustomRepository.java
+++ backend/spring/src/main/java/com/example/back/repository/CustomRepository/InsertCustomRepository.java
@@ -7,7 +7,6 @@ import com.example.back.model.post.PostPermission;
 import com.example.back.model.post.Posts;
 import com.example.back.model.user.UserAuth;
 import com.example.back.model.user.UserInformation;
-import com.example.back.model.user.Users;
 
 import org.springframework.stereotype.Repository;
 
@@ -15,16 +14,12 @@ import org.springframework.stereotype.Repository;
 public interface InsertCustomRepository {
 
     void saveSignUpUserInfo(UserInformation userInfo);
-    void saveSignUpUserInfo(Users user) throws SQLException;
     
     //void saveLoginUserInfo(UserInformation dto);
-    
-    void saveUserToken(UserAuth token);
+    //void saveUserToken(UserAuth token);
     
     void savePostPermission(PostPermission permission);
 
     void savePostInformation(PostInformation postInfo) throws SQLException;
-    void savePosts(Posts post) throws SQLException;
-
-    
+    void savePosts(Posts postInfo) throws SQLException;
 }
